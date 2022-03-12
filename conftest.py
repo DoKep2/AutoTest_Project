18240e2e7d5eaf26fbb5780fc1bf084a57a581c0
Sergo <sergo.elisbar@yandex.ru>
2022-03-12 15:47:34 +0300

    add conftest.py

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+def pytest_addoption(parser):
+    parser.addoption("--browser_name", action='store', default='chrome',
+        help='Choose necessary browser')
+    parser.addoption("--language", action='store', default='en',
+        help='Choose necessary language')
+@pytest.fixture
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option("prefs", {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == 'firefox':
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        pass
+    yield browser
+    time.sleep(3)
+    browser.quit()
