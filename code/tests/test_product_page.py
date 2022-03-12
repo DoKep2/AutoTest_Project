e12b16df46503c0abe9d6ce1762eb1b1365836f7
Sergo <sergo.elisbar@yandex.ru>
2022-03-13 00:27:13 +0300

    move common methods from main page to base page

--- code/tests/test_product_page.py
+++ code/tests/test_product_page.py
@@ -1,7 +1,8 @@
 import pytest
 from pages.product_page import ProductPage
 
-#link = "https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+
+# link = "https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
 
 
 @pytest.mark.skip
@@ -19,12 +20,14 @@ def test_can_add_product_to_cart(browser):
     page.open()
     page.add_product_to_cart()
 
+
 @pytest.mark.skip
 def test_add_product_with_corresponding_name(browser):
     link = "https://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     page.added_product_with_corresponding_name()
+
 
 @pytest.mark.skip
 def test_add_product_with_corresponding_price(browser):
@@ -42,12 +45,13 @@ def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
     page.add_product_to_cart()
     page.should_not_be_success_message()
 
-
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     link = "https://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
+
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
@@ -74,3 +78,16 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.open()
     page.added_product_with_corresponding_name()
 
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
