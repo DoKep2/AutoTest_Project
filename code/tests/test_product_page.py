75c83d25b1faa0638b2c5719607989e941b2104e
Sergo <sergo.elisbar@yandex.ru>
2022-03-13 16:15:07 +0300

    add cart page & negative checks

--- code/tests/test_product_page.py
+++ code/tests/test_product_page.py
@@ -1,4 +1,6 @@
 import pytest
+
+from pages.cart_page import CartPage
 from pages.product_page import ProductPage
 
 
@@ -37,13 +39,15 @@ def test_add_product_with_corresponding_price(browser):
     page.added_product_with_corresponding_price()
 
 
-@pytest.mark.xfail
+# @pytest.mark.xfail
+@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
     link = "https://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_cart()
     page.should_not_be_success_message()
+
 
 @pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
@@ -53,7 +57,8 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
-@pytest.mark.xfail
+#@pytest.mark.xfail
+@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "https://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
@@ -78,16 +83,34 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.open()
     page.added_product_with_corresponding_name()
 
-
+@pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.skip
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_should_see_empty_cart_title_from_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_cart_page()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.should_be_empty_cart_title()
+
+
+def test_product_in_cart_should_disappear(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_cart_page()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.should_disappear_cart_items()
